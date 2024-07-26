7d454e6bf1aaf184f0a1b4611bde3b5db51a5789
madhav <163291389+madhavsGIT@users.noreply.github.com>
2024-07-26 14:38:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const inputField = document.getElementById('fname');
+
+    inputField.addEventListener('blur', function() {
+        inputField.value = inputField.value.toUpperCase();
+    });
+});
